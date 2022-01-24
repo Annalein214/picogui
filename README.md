6f7e42e722fbdbd410b2fba553b40071649980fc
Anna Pollmann <aobertacke@icecube.wisc.edu>
2022-01-25 00:24:55 +0100

    filterwheel now with classes and cleaned up

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # picogui
 Python GUI for the Picoscope and some other hardware
+
+
+
+# Further hardware
+
+## Filterwheel: 
+
+A motor is driven by an Arduino and its position read out with an Encoder. The motor turns a wheel with optical filters in it beneath a PMT. So the position of the filters needs to be accurately adjusted. 
+Currently this is an independent program which requires independent setup, see this file.
+
--- filterwheel/Filterwheel.py
+++ filterwheel/Filterwheel.py
@@ -1,11 +1,46 @@
+###################################################################################################
+''' Readme
+This script lets an arduino drive a motor while it uses an encoder for precise positioning.
+
+Make sure you adjust for the correct temperature (configure option "cold")
+
+Make sure you know in which path you can find which serial device (Arduino & Encoder).
+Plug them in one after each other and in between execute in terminal:
+$ ls -al /dev/ttyUSB*
+Fill the correct paths into the configuration section below
+
+
+'''
+
+
+###################################################################################################
+
 from __future__ import print_function
 from __future__ import absolute_import
 
-import serial
-import time
+import serial, time, sys, traceback, os
+sys.path.append("../code/")
+from log import log
+
+###################################################################################################
+# configuration
+arduino_path="/dev/ttyUSB1"
+encoder_path="/dev/ttyUSB0"
+
+log_dir=os.getcwd() # current directory
+
+log_level="debug" # debug, info
+
+test_with_short_times=True 
+
+cold=False
+
+
+###################################################################################################
+# Definitions
 
 # bits to setup the encoder
-SETUP_P34=bytearray([2, '1', 0x59, 0x50, 0x33, 0x34, 0x53, 0x32, 0x3A, 0x58, 0x58, 0x03, 13, 10])
+SETUP_P34=bytearray([2, 1, 0x59, 0x50, 0x33, 0x34, 0x53, 0x32, 0x3A, 0x58, 0x58, 0x03, 13, 10])
 SETUP_P35=bytearray([2, 0x31, 0x59, 0x50, 0x33, 0x35, 0x53, 0x31, 0x31, 0x3A, 0x58, 0x58, 0x03, 13, 10])
 # command to readposition
 READPOSITION=bytearray([2, 0x31, 0x59, 0x50, 0x32, 0x32, 0x52, 0x3A, 0x58, 0x58, 0x03, 13, 10])
@@ -29,178 +64,215 @@ FILTERS = [FILTER450, FILTER475, FILTER500, CLOSED, OPEN, FILTER325, FILTER350, 
 NEXT = "0745"
 ADJUST = "0010"
 STEP = "0005"
-
-# steps at cold temperatures
-NEXT_cold= "1045"
-ADJUST_cold="0015"
-STEP_cold="0007"
+if cold:
+    # steps at cold temperatures
+    NEXT= "1045"
+    ADJUST="0015"
+    STEP="0007"
 
 ERRCNT = 5
 
+if test_with_short_times:
+    DELAY=10
+else:
+    DELAY=3600
+###################################################################################################
+# arduino
 
-def waitForArduino(Arduino):
-    # wait for Arduino response after boot up
-    msg=""
-    print("Waiting for Arduino...")
-    while msg.find("Arduino is ready") == -1:
-        while Arduino.inWaiting() == 0:
-            pass
+class Arduino:
 
-        msg = str(Arduino.readline())
-        print("Wait for Arduino:%s"%msg)
+    def __init__(self,path, encoder, log):
+        self.path=path
+        self.log=log
+        self.encoder=encoder
 
-def driveMotor(Arduino, x):
-    #print("Drive step: %s"%x)
-    cmmd=x+"\n"
-    Arduino.write(cmmd)
-    return Arduino.readline()
+    def connect(self,):
+        self.arduino=serial.Serial(self.path, 9600, timeout=10)
+        self.log.debug("Initiated Arduino")
+        waitForArduino(self.arduino)
+        self.log.info("Arduino ready!")
 
+    def close_connection(self):
+        self.arduino.close()
+        self.log.info("Connection to Arduino closed!")
 
-def setupEncoder(Encoder):
-    print("Setting up Encoder...")
-    Encoder.write(SETUP_P34)
-    Encoder.readline()
-    print("Initialising Encoder...")
-    Encoder.write(SETUP_P35)
-    Encoder.readline()
-    print("Encoder is ready!")
-
-def readPosition(Encoder):
-    Encoder.write(READPOSITION)
-    pos=Encoder.readline()
-    #print("Read Position: %s"%str(pos))
-    pos=pos[2:-6]
-    print("Read Position: %s"%str(pos))
-    return pos
-
-def getSign(x):
-    if x<0:
-        return "-"
-    else:
-        return "+"
-
-def adjustPosition(Arduino, Encoder, pos, adj, stp):
-    adjPos=pos
-    foundPos=False
-
-    while foundPos==False:
-        cmmdstr=""
-        diff=[]
-        sign=[]
-
-        #print("Wrong position, searching closest filter...")
-        # find the filter which is closest
-        for i in range(len(FILTERS)):
-            diff.append(abs(FILTERS[i]-int(adjPos)))
-            sign.append(getSign(FILTERS[i]-int(adjPos)))
-        if diff[1] > 1000:
-            diff[1]=abs(diff[0]-2048)
-            sign[1]="+"
-
-        
-        filtndx=diff.index(min(diff))
-        #print("Closest filter is %d (index %d)"%(FILTERS[filtndx],filtndx))
-
-        # adjust direction
-        cmmdstr+=sign[filtndx]
-
-        # step size normal or small if very close
-        if diff[filtndx]<4:
-            cmmdstr+=stp
-        else:
-            cmmdstr+=adj
-        #print(filtndx)
-        #print(diff[filtndx])
-        #print(cmmdstr)
-
-        # execute
-        driveMotor(Arduino, cmmdstr)
-        adjPos=int(readPosition(Encoder))
-        #print("Adjust position from:",adjPos, " with step:",cmmdstr)
-
-        if adjPos==FILTERS[filtndx]:
-            foundPos=True
-            #print("Found position")
-            return adjPos
-        elif adjPos==2047:
-            ERRCNT-=1
-            if ERRCNT == 0:
-                raise RuntimeError
-        else:
-            #print("Position not found")
-            time.sleep(1)
-
-def connect_to_arduino():
-    arduino=serial.Serial('/dev/ttyUSB1', 9600, timeout=10)
-    print ("Initiated Arduino")
-    waitForArduino(arduino)
-    print ("Arduino ready!")
-    return arduino
-
-def connect_to_encoder():
-    encoder=serial.Serial('/dev/ttyUSB0', 115200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
-    print ("Initiated Encoder.")
-    setupEncoder(encoder)
-    print ("Encoder ready!")
-    return encoder
-
-def close_connection(dev):
-    dev.close()
-    print("Connection closed!")
-
-def filterwheel(arduino, encoder):
-    Data = []
-    t=time.strftime("%Y_%m_%d_%H_%M_%S")
-    Data.append(t)
-    pos_old=readPosition(encoder)
-    Data.append(pos_old)
-    print("Old Position",pos_old)
+    def waitForArduino(self):
+        # wait for Arduino response after boot up
+        msg=""
+        self.log.debug("Waiting for Arduino...")
+        while msg.find("Arduino is ready") == -1:
+            while self.arduino.inWaiting() == 0:
+                pass
+            msg = str(self.arduino.readline())
+            self.log.debug("Wait for Arduino: %s"%msg)
 
 
-    for n in range(30):
+    def driveMotor(self, x):
+        #print("Drive step: %s"%x)
+        cmmd=x+"\n"
+        self.arduino.write(cmmd)
+        return self.arduino.readline()
 
-        if 1:
-            nextfilter=NEXT
-            adjustfilter=ADJUST
-            stepfilter=STEP
-        else:
-            nextfilter=NEXT_cold
-            adjustfilter=ADJUST_cold
-            stepfilter=STEP_cold
+    def run(self):
 
-        Data.append(time.strftime("%Y_%m_%d_%H_%M_%S"))
-        done=driveMotor(arduino, "+"+nextfilter)
-        done=done.strip('\n')
-        Data.append(done)
-        pos_new=readPosition(encoder)
-        Data.append(pos_new)
-        adaPos=adjustPosition(arduino, encoder, pos_new, adj=adjustfilter, stp=stepfilter)
-        Data.append(adaPos)
-        Data.append(time.strftime("%Y_%m_%d_%H_%M_%S"))
-
-        with open("motorlog_"+t+".txt", 'a') as f:
-            for item in Data:
-                f.write("%s\n" % item)
-        Data=[]
-        time.sleep(10)
+        # make log entry
+        log_string=""
+        pos_old=readPosition(encoder)
+        log_string+=str(pos_old)+"; "
+        self.log.debug("Old Position",pos_old)
 
 
+        for n in range(50): 
+
+            log_string+="%s; " % str(time.strftime("%Y_%m_%d_%H_%M_%S"))
+
+            drive=self.arduino.driveMotor("+"+NEXT)
+            log_string+="%s; " % (drive.strip('\n'))
+            pos_new=self.encoder.readPosition()
+            log_string+="%s; " % (str(pos_new))
+            adjPos=self.adjustPosition(pos_new, adj=ADJUST, stp=STEP) ###############
+            log_string+="%s; " % str(adaPos)
+
+            log_string+="%s; " % str(time.strftime("%Y_%m_%d_%H_%M_%S"))
+
+            self.log.info(log_string)
+            log_string=""
+            time.sleep(DELAY)
+
+    def adjustPosition(self, pos, adj, stp):
+
+        def getSign(x):
+            if x<0:
+                return "-"
+            else:
+                return "+"
+
+        adjPos=pos
+        foundPos=False
+
+        while foundPos==False:
+            cmmdstr=""
+            diff=[]
+            sign=[]
+
+            self.log.debug("Wrong position, searching closest filter...")
+            # find the filter which is closest
+            for i in range(len(FILTERS)):
+                diff.append(abs(FILTERS[i]-int(adjPos)))
+                sign.append(getSign(FILTERS[i]-int(adjPos)))
+            if diff[1] > 1000:
+                diff[1]=abs(diff[0]-2048)
+                sign[1]="+"
+
+            
+            filtndx=diff.index(min(diff))
+            self.debug("Closest filter is %d (index %d)"%(FILTERS[filtndx],filtndx))
+
+            # adjust direction
+            cmmdstr+=sign[filtndx]
+
+            # step size normal or small if very close
+            if diff[filtndx]<4:
+                cmmdstr+=stp
+            else:
+                cmmdstr+=adj
+
+            # execute
+            self.arduino=driveMotor(cmmdstr)
+            adjPos=int(self.encoder.readPosition())
+            self.log.debug("Adjust position from:",adjPos, " with step:",cmmdstr)
+
+            if adjPos==FILTERS[filtndx]:
+                foundPos=True
+                self.log.debug("Found position")
+                return adjPos
+            elif adjPos==2047:
+                ERRCNT-=1
+                if ERRCNT == 0:
+                    raise RuntimeError
+            else:
+                self.log.debug("Position not found")
+                time.sleep(1)
+
+
+###################################################################################################
+# encoder
+class Encoder:
+    def __init__(self,path, log):
+        self.path=path
+        self.log=log
+
+    def connect(self):
+        self.encoder=serial.Serial(self.path, 115200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
+        self.log.debug ("Initiated Encoder.")
+        setupEncoder(self.encoder)
+        self.log.info ("Encoder ready!")
+
+    def setupEncoder(self):
+        self.log.debug("Setting up Encoder...")
+        self.encoder.write(SETUP_P34)
+        self.encoder.readline()
+        self.log.debug("Initialising Encoder...")
+        self.encoder.write(SETUP_P35)
+        self.encoder.readline()
+        self.log.debug("Encoder is ready!")
+
+    def close_connection(self):
+        if hasattr(self, 'encoder'):
+            self.log.info("Connection to Encoder closed!")
+
+    def readPosition(self):
+        self.encoder.write(READPOSITION)
+        pos=self.encoder.readline()
+        #print("Read Position: %s"%str(pos))
+        pos=pos[2:-6] # rest is rubbish
+        print("Read Position: %s"%str(pos))
+        return pos
+
+###################################################################################################
+# example how to run:
 
 if __name__=="__main__":
-    while(True):
+
+    logger=log(save=True, level=log_level, directory=log_dir, 
+                    end="fw.log", # use different ending than pico main script, to make sure there is no override
+                    )
+    while(True): # use this in case there is suddenly a deconnection of devices
+        
         try:
-            Arduino = connect_to_arduino()
-            Encoder = connect_to_encoder()
-            #time.sleep(3600)
-            #time.sleep(20)
-            filterwheel(Arduino, Encoder)
+            
+            encoder=Encoder(encoder_path, logger)
+            encoder.connect()
+
+            arduino=Arduino(arduino_path, encoder, logger)
+            arduino.connect()
+
+            time.sleep(DELAY)            
+            
+            arduino.run(Arduino, Encoder)
+            
             break
         except serial.SerialException as e:
-            print ("ERROR:", e )
-            close_connection(Arduino)
-            close_connection(Encoder)
+            traceback.print_exc()
+            if 'arduino' in globals(): arduino.close_connection()
+            if 'encoder' in globals(): encoder.close_connection()
             continue
         except (KeyboardInterrupt, SystemExit, RuntimeError):
-            close_connection(Arduino)
-            close_connection(Encoder)
+            traceback.print_exc()
+            if 'arduino' in globals(): arduino.close_connection()
+            if 'encoder' in globals(): encoder.close_connection()
             break
+
+###################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
